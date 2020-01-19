ea672260ff9c8587c8c22547803cb988053b76a1
tklos <tomasz.klos9@gmail.com>
2020-01-19 11:09:55 +0100

    Add admin for devices

---
+++ collect/apps/devices/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+
+from .models import Device
+
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = (
+        'get_str',
+        'user',
+        'sequence_id',
+        'name',
+        'date_added',
+    )
+    list_display_links = list_display
+    list_filter = (
+        'user',
+    )
+    fields_create = (
+        'user',
+        'name',
+    )
+    fields_modify = (
+        'user',
+        'name',
+        'token',
+        'date_added',
+    )
+    readonly_fields = (
+        'token',
+        'date_added',
+    )
+
+    def get_str(self, obj):
+        return str(obj)
+    get_str.short_description = 'str'
+
+    def get_fields(self, request, obj=None):
+        return self.fields_create if obj is None else self.fields_modify
+
---
+++ collect/apps/lib/settings/admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+
+from django.conf.locale.en import formats as en_formats
+en_formats.DATETIME_FORMAT = 'Y-m-d H:i:s'
+
+
+ordering = {
+    'Users': 0,
+    'Devices': 1,
+}
+
+
+def get_app_list(self, request):
+    """
+    Return a sorted list of all the installed apps that have been
+    registered in this site.
+    """
+    app_dict = self._build_app_dict(request)
+    app_dict = {k: v for k, v in app_dict.items() if v['name'] in ordering}
+
+    # Sort the apps alphabetically.
+    # app_list = sorted(app_dict.values(), key=lambda x: x['name'].lower())
+    app_list = sorted(app_dict.values(), key=lambda x: ordering[x['name']])
+
+    # Sort the models alphabetically within each app.
+    for app in app_list:
+        app['models'].sort(key=lambda x: x['name'])
+
+    return app_list
+
+
+import types
+admin.site.get_app_list = types.MethodType(get_app_list, admin.site)
+
