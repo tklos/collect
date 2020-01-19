ff888f6f036c2a5708738cf26e8ddd6ae8442be5
tklos <tomasz.klos9@gmail.com>
2020-01-19 10:14:37 +0100

    Add adding devices

--- collect/settings/base.py
+++ collect/settings/base.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'users',
+    'devices',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +69,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': {
+                'common_tags': 'lib.templatetags.common_templatetags',
+            },
         },
     },
 ]
