71045008245e85a64fa1bc2bf16f5a2051431662
tklos <tomasz.klos9@gmail.com>
2020-01-19 11:59:10 +0100

    Add measurements app

--- collect/apps/lib/templatetags/common_templatetags.py
+++ collect/apps/lib/templatetags/common_templatetags.py
@@ -8,3 +8,9 @@ register = template.Library()
 def get_at_index(obj, index):
     return obj[index]
 
+
+@register.filter
+def order_by(obj, args):
+    args = args.split(',')
+    return obj.order_by(*args)
+
--- collect/settings/base.py
+++ collect/settings/base.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'lib.settings',
     'users',
     'devices',
+    'measurements',
 ]
 
 MIDDLEWARE = [
