23f65df66d87e6bab08cbe944ccc8ee69504a18a
tklos <tomasz.klos9@gmail.com>
2020-01-19 09:10:08 +0100

    Add user accounts

--- collect/settings/base.py
+++ collect/settings/base.py
@@ -87,21 +87,18 @@ AUTH_USER_MODEL = 'users.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'OPTIONS': {
+            'min_length': 6,
+        },
     },
 ]
+
+# Login
+LOGIN_URL = 'account:login'
+LOGIN_REDIRECT_URL = 'profile:home'
 
 
 # Internationalization
@@ -116,6 +113,13 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+
+# Messages
+from django.contrib import messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
 
 
 # Static files (CSS, JavaScript, Images)
--- collect/urls.py
+++ collect/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='profile:home'), name='index'),
+
+    path('account/', include('accounts.urls')),
+    path('profile/', include('profiles.urls')),
 
     path('django-admin/', admin.site.urls),
 ]
