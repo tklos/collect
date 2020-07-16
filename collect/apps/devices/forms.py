fabd4409203385268896640a5d2e95f52842b766
tklos <tomasz.klos9@gmail.com>
2020-07-16 17:15:56 +0200

    Allow offset as date-from

--- collect/apps/devices/forms.py
+++ collect/apps/devices/forms.py
@@ -41,22 +41,35 @@ class DevicePlotDateForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        # Current time
+        self.current_time = datetime.now(settings.LOCAL_TIMEZONE)
+        self.date_to_is_now = False
+
+        # Set widget attributes
         text_field_attrs = {
             'class': 'form-control input-sm input-font-size-14 width-input-date',
             'autocomplete': 'off',
         }
 
         self.fields['date_from'].widget.attrs.update(text_field_attrs)
-        self.fields['date_from'].widget.attrs['placeholder'] = 'datetime'
+        self.fields['date_from'].widget.attrs['placeholder'] = 'datetime or offset'
 
         self.fields['date_to'].widget.attrs.update(text_field_attrs)
         self.fields['date_to'].widget.attrs['placeholder'] = 'datetime or "now"'
 
     def clean_date_from(self):
+        """
+        Returns timedelta if date_from is an offset, datetime otherwise
+        """
         s = self.cleaned_data['date_from'].strip()
 
+        prefix, suffix = '-', 'hours'
         try:
-            date_from = settings.LOCAL_TIMEZONE.localize(datetime.strptime(s, '%Y-%m-%d %H:%M'))
+            if s.startswith(prefix) and s.endswith(suffix):
+                offset_h = float(s[len(prefix):-len(suffix)])
+                date_from = timedelta(hours=offset_h)
+            else:
+                date_from = settings.LOCAL_TIMEZONE.localize(datetime.strptime(s, '%Y-%m-%d %H:%M'))
 
         except Exception as exc:
             raise ValidationError(f'Can\'t parse date: {exc}')
@@ -68,7 +81,8 @@ class DevicePlotDateForm(forms.Form):
 
         try:
             if s == 'now':
-                date_to = datetime.now(settings.LOCAL_TIMEZONE).replace(second=0, microsecond=0) + timedelta(minutes=1)
+                date_to = self.current_time.replace(second=0, microsecond=0) + timedelta(minutes=1)
+                self.date_to_is_now = True
             else:
                 date_to = settings.LOCAL_TIMEZONE.localize(datetime.strptime(s, '%Y-%m-%d %H:%M'))
 
@@ -76,4 +90,24 @@ class DevicePlotDateForm(forms.Form):
             raise ValidationError(f'Can\'t parse date: {exc}')
 
         return date_to
+
+    def clean(self):
+        super().clean()
+
+        # Check dates
+        date_from, date_to = self.cleaned_data.get('date_from'), self.cleaned_data.get('date_to')
+        if date_from is not None and date_to is not None:
+            if isinstance(date_from, timedelta):
+                try:
+                    date_from = date_to - date_from
+                except Exception as exc:
+                    raise ValidationError(f'Can\'t create date-from: {exc}')
+
+                if self.date_to_is_now:
+                    date_from -= timedelta(minutes=1)
+
+            if date_to <= date_from:
+                self.add_error(None, 'date-to should be later than date-from')
+
+            self.cleaned_data['date_from'] = date_from
 
