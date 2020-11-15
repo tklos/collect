00568de4510ba63d78b5adc37e84533bc4c45427
tklos <tomasz.klos9@gmail.com>
2020-11-15 19:09:13 +0100

    Add number of measurements to devices in profile page

--- collect/apps/profiles/views.py
+++ collect/apps/profiles/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import get_user_model
+from django.db.models import Count
 from django.views.generic import TemplateView
 
 from devices.forms import DeviceAddForm
@@ -7,9 +9,18 @@ class ProfileView(TemplateView):
     template_name = 'profiles/home.html'
 
     def get_context_data(self, **kwargs):
+        devices = (
+            self.request.user
+            .device_set
+            .annotate(
+                Count('measurement_set'),
+            )
+        )
+
         context = super().get_context_data(**kwargs)
         context.update({
             'device_add_form': DeviceAddForm(),
+            'devices': devices,
         })
 
         return context
