157bb83ea377a63334839b685519ba5157031d7e
tklos <tomasz.klos9@gmail.com>
2026-06-13 10:11:29 +0200

    Add bulk delete

--- collect/static/js/js.js
+++ collect/static/js/js.js
@@ -33,6 +33,74 @@ $(document).ready(function() {
 	});
 
 
+	/* Bulk delete: select-all checkbox */
+	$("body").on("change", ".measurement-select-all", function () {
+		var checked = $(this).prop("checked");
+		$(this).closest("table").find(".measurement-select-checkbox").prop("checked", checked);
+		updateBulkDeleteButton($(this).closest(".div-ajax, .div-measurements-top-controls").closest("div, body"));
+	});
+
+	/* Bulk delete: individual checkbox with shift-click range selection */
+	var lastCheckedMeasurement = null;
+	$("body").on("click", ".measurement-select-checkbox", function (event) {
+		var checkboxes = $(this).closest("table").find(".measurement-select-checkbox");
+		if (event.shiftKey && lastCheckedMeasurement && lastCheckedMeasurement.closest("table")[0] === $(this).closest("table")[0]) {
+			var all = checkboxes.toArray();
+			var start = all.indexOf(lastCheckedMeasurement[0]);
+			var end = all.indexOf(this);
+			if (start > end) { var tmp = start; start = end; end = tmp; }
+			var targetState = $(this).prop("checked");
+			for (var i = start; i <= end; i++) {
+				$(all[i]).prop("checked", targetState);
+			}
+		}
+		lastCheckedMeasurement = $(this);
+		var table = $(this).closest("table");
+		var all = table.find(".measurement-select-checkbox");
+		var allChecked = all.length === all.filter(":checked").length;
+		table.find(".measurement-select-all").prop("checked", allChecked);
+		updateBulkDeleteButton($(this).closest(".div-ajax, body"));
+	});
+
+	function updateBulkDeleteButton(scope) {
+		var anyChecked = $(".measurement-select-checkbox:checked").length > 0;
+		$(".btn-bulk-delete-measurements").prop("disabled", !anyChecked);
+	}
+
+	/* Bulk delete: delete button */
+	$("body").on("click", ".btn-bulk-delete-measurements", function (event) {
+		var checked = $(".measurement-select-checkbox:checked");
+		if (checked.length === 0) return;
+
+		var btn = $(this);
+		btn.prop("disabled", true).text("Deleting...");
+
+		var promises = checked.toArray().map(function (cb) {
+			var $cb = $(cb);
+			var tr = $cb.closest("tr");
+			return $.ajax({
+				type: "POST",
+				url: $cb.data("url"),
+				headers: {
+					"X-CSRFToken": $cb.data("csrftoken"),
+				},
+				contentType: "application/json; charset=utf-8",
+				dataType: "json",
+			}).done(function () {
+				tr.addClass("strikeout");
+				$cb.prop("checked", false);
+			}).fail(function (data) {
+				alert("Request failed (error " + data.status + ": " + data.statusText + "); please reload page");
+			});
+		});
+
+		$.when.apply($, promises).always(function () {
+			btn.text("Delete selected");
+			updateBulkDeleteButton();
+		});
+	});
+
+
 	/* Data panel */
 	$("body").on("click", ".measurement-delete-link", function (event) {
 		event.preventDefault();
